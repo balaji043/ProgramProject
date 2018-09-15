c112e40a042780b035ab6bc7aae5bfd6f3b1a39a
balaji043 <cubanhog.royalgamma@gmail.com>
2018-09-15 11:00:54 +0530

    pyramid

--- src/Pyramid_Pattern.java
+++ src/Pyramid_Pattern.java
@@ -9,13 +9,11 @@ public class Pyramid_Pattern {
             for (j = 0; j < n - i - 1; j++) {
                 System.out.print("  ");
             }
-
             for (k = 0; k < d; k++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-
     }
 }
 /*
