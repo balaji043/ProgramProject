b306701ba350888dd5e935c616ad2197cf4e3016
balaji043 <cubanhog.royalgamma@gmail.com>
2018-09-15 21:03:42 +0530

    Wipro robot 90 + 45 degree 1 or 2 movement

--- src/Wipro_Robot.java
+++ src/Wipro_Robot.java
@@ -1,102 +1,193 @@
 import java.util.Scanner;
 
 class Wipro_Robot {
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         String[] line1 = scanner.nextLine().split(" ");
-        String[] line2 = scanner.nextLine().split("-");
-        String[] line3 = scanner.nextLine().split(" ");
+        String line2 = scanner.nextLine();
+        String line3 = scanner.nextLine();
 
         int r = Integer.parseInt(line1[0]);
         int c = Integer.parseInt(line1[1]);
 
-        System.out.println(moveRobot(r, c, line2, line3));
+        System.out.println(Robot_90_45_Movement.moveRobot(r, c, line2, line3));
     }
+}
 
-    private static String moveRobot(int rows, int columns, String[] input3, String[] input4) {
-        int x = Integer.parseInt(input3[0]), y = Integer.parseInt(input3[1]), a, b;
-        String facing = "" + input3[2], f = facing;
-        System.out.println("x = " + x + ",y = " + y);
-        for (String op : input4) {
+class Robot_90_45_Movement {
+    static String moveRobot(int r, int c, String input3, String input4) {
+        int a, b, u;
+        String[] position = input3.split("-");
+        int x = Integer.parseInt(position[0]);
+        int y = Integer.parseInt(position[1]);
+        String facing = position[2], f;
+        String[] moves = input4.split(" ");
+        for (String move : moves) {
             a = x;
             b = y;
-            if (isTurn(op)) {
-                f = facing;
-                facing = getFacing(op, facing);
-                System.out.println(op + " Turn \n" + f + " -> " + facing);
+            f = facing;
+            if (isTurn(move)) {
+                facing = getFacing(move, facing);
             } else {
-                System.out.println("Move");
+                if (move.equals("M")) u = 2;
+                else u = 1;
                 switch (facing) {
                     case "N": {
-                        y++;
+                        y += u;
                         break;
                     }
-                    case "S": {
-                        y--;
+                    case "NE": {
+                        y += u;
+                        x += u;
                         break;
                     }
                     case "E": {
-                        x++;
+                        x += u;
+                        break;
+                    }
+                    case "SE": {
+                        y -= u;
+                        x += u;
+                        break;
+                    }
+                    case "S": {
+                        y -= u;
+                        break;
+                    }
+                    case "SW": {
+                        y -= u;
+                        x -= u;
                         break;
                     }
                     case "W": {
-                        x--;
+                        x -= u;
+                        break;
+                    }
+                    case "NW": {
+                        y += u;
+                        x -= u;
                         break;
                     }
                 }
-                System.out.println("x = " + x + ",y = " + y);
-                if (!isValid(x, y, rows, columns)) return "" + a + "-" + b + "-" + f + "-" + "ER";
+                if (!isValid(x, y, r, c)) {
+                    return "" + a + "-" + b + "-" + f + "-ER";
+                }
             }
         }
         return "" + x + "-" + y + "-" + facing;
     }
 
-    private static boolean isValid(int x, int y, int r, int c) {
-        boolean ans = false;
-        if (x >= 0 && y >= 0 && x <= r && y <= c)
-            ans = true;
-        return ans;
+    private static boolean isTurn(String op) {
+        return op.equals("R") || op.equals("L") || op.equals("l") || op.equals("r");
     }
 
-    private static boolean isTurn(String op) {
-        return op.equals("R") || op.equals("L");
+    private static boolean isValid(int x, int y, int r, int c) {
+        return (x >= 0 && x <= r && y >= 0 && y <= c);
     }
 
     private static String getFacing(String op, String facing) {
         switch (facing) {
             case "N": {
-                if (op.equals("R"))
-                    facing = "E";
-                else
-                    facing = "W";
-                break;
+                switch (op) {
+                    case "R":
+                        return "E";
+                    case "r":
+                        return "NE";
+                    case "L":
+                        return "W";
+                    default:
+                        return "NW";
+                }
+            }
+            case "NE": {
+                switch (op) {
+                    case "R":
+                        return "SE";
+                    case "r":
+                        return "E";
+                    case "L":
+                        return "NW";
+                    default:
+                        return "N";
+                }
             }
             case "E": {
-                if (op.equals("R"))
-                    facing = "S";
-                else
-                    facing = "N";
-                break;
+                switch (op) {
+                    case "R":
+                        return "S";
+                    case "r":
+                        return "SE";
+                    case "L":
+                        return "N";
+                    default:
+                        return "NE";
+                }
             }
-            case "W": {
-                if (op.equals("R"))
-                    facing = "N";
-                else
-                    facing = "S";
-                break;
+            case "SE": {
+                switch (op) {
+                    case "R":
+                        return "SW";
+                    case "r":
+                        return "S";
+                    case "L":
+                        return "NE";
+                    default:
+                        return "E";
+                }
             }
             case "S": {
-                if (op.equals("R"))
-                    facing = "W";
-                else
-                    facing = "E";
-                break;
+                switch (op) {
+                    case "R":
+                        return "W";
+                    case "r":
+                        return "SW";
+                    case "L":
+                        return "E";
+                    default:
+                        return "SE";
+                }
+            }
+            case "SW": {
+                switch (op) {
+                    case "R":
+                        return "NW";
+                    case "r":
+                        return "W";
+                    case "L":
+                        return "SE";
+                    default:
+                        return "S";
+                }
+
+            }
+            case "W": {
+                switch (op) {
+                    case "R":
+                        return "N";
+                    case "r":
+                        return "NW";
+                    case "L":
+                        return "S";
+                    default:
+                        return "SW";
+                }
+            }
+            case "NW": {
+                switch (op) {
+                    case "R":
+                        return "NE";
+                    case "r":
+                        return "N";
+                    case "L":
+                        return "SW";
+                    default:
+                        return "W";
+                }
             }
         }
-        return facing;
+        return "";
     }
 }
 
