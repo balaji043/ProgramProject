e0ed8687456b40ec5944008293006b8c8ffa50e8
Aakash <aakash@gmail.com>
2018-09-23 23:11:48 +0530

    Queues using stack

--- src/Stack_And_Queue.java
+++ src/Stack_And_Queue.java
@@ -1,8 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Stack_And_Queue {
     public static void main(String[] args) {
         new Queue_Using_Stacks();
+        new Stack_Using_Queues();
     }
 }
 
@@ -11,23 +14,66 @@ class Queue_Using_Stacks {
     private Stack<Integer> stack2 = new Stack<>();
 
     Queue_Using_Stacks() {
-        Enqueue(10);
-        Enqueue(11);
-        Enqueue(12);
-        System.out.println(Dequeue());
+        Enqueue(1);
+        Enqueue(2);
+        Enqueue(3);
+        Dequeue();
+        Dequeue();
+        Dequeue();
+        Dequeue();
     }
 
     private void Enqueue(int n) {
+        System.out.println("Enqueue : " + n);
         stack1.push(n);
     }
 
-    private String Dequeue() {
+    private void Dequeue() {
+        String a;
         if (stack1.size() == 0 && stack2.size() == 0)
-            return "Queue is empty";
-        while (!stack1.empty()) {
-            int p = stack1.pop();
-            stack2.push(p);
+            a = "Queue is empty";
+        else {
+            while (!stack1.empty()) {
+                int p = stack1.pop();
+                stack2.push(p);
+            }
+            a = "Dequeue : " + stack2.pop();
         }
-        return "" + stack2.pop();
+        System.out.println(a);
+    }
+}
+
+class Stack_Using_Queues {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    Stack_Using_Queues() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+        push(1);
+        push(2);
+        push(3);
+        pop();
+        pop();
+        pop();
+        pop();
+    }
+
+    private void push(int data) {
+        System.out.println("push : " + data);
+        queue1.add(data);
+    }
+
+    private void pop() {
+        if (queue1.size() < 1) {
+            System.out.println("Nothing to pop");
+            return;
+        }
+        while (queue1.size() > 1) {
+            queue2.add(queue1.remove());
+        }
+        System.out.println("pop  : " + queue1.remove());
+        queue1 = queue2;
+        queue2 = new LinkedList<>();
     }
 }
