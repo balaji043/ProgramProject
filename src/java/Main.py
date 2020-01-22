e0387fd1665d50b476fd4cfba7249653093be09b
Balaji Ramasamy <cubanhog.royalgamma@gmail.com>
2020-01-22 23:51:56 +0530

    Lab exercises

---
+++ src/java/Main.py
@@ -0,0 +1,3 @@
+import numpy as np
+a=np.array([10,8,9])
+print(a)
---
+++ src/java/lab/SHA_Secure_Hash_Algrithm.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SHA_Secure_Hash_Algrithm {
+    public static void main(String[] args) throws IOException {
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter string:");
+        String rawString = userInput.readLine();
+
+        /*try {
+           /System.out.println("SHA1 hash of string: " + AeSimpleSHA1.SHA1(rawString));
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+    }
+}
