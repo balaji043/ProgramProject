642c01cde7c3db8d5fa0b465fa5fa1cb97cbbf37
Balaji Ramasamy <cubanhog.royalgamma@gmail.com>
2020-04-04 02:06:17 +0530

    added decription for the HappyNumber.

--- src/leetcode/week1/HappyNumber.java
+++ src/leetcode/week1/HappyNumber.java
@@ -1,7 +1,19 @@
+import java.util.logging.Logger;
+
+/**
+ * we will keep two numbers slow and fast both initialize from a given number,
+ * slow is replaced one step at a time and fast is replaced two steps at a time.
+ * If they meet at 1, then the given number is Happy Number otherwise not.
+ * Input: n = 19 Output: True 19 is Happy Number, 1^2 + 9^2 = 82 8^2 + 2^2 = 68
+ * 6^2 + 8^2 = 100 1^2 + 0^2 + 0^2 = 1
+ */
 public class HappyNumber {
+
+    private static final Logger logger = Logger.getLogger(HappyNumber.class.getName());
+
     public static void main(String[] args) {
-        // System.out.println(isHappyNumber(20));
-        System.out.println(isHappyNumber(19));
+        logger.info("" + isHappyNumber(20));
+        logger.info("" + isHappyNumber(19));
     }
 
     private static boolean isHappyNumber(int n) {
